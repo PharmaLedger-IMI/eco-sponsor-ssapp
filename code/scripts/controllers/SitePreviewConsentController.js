48ee72cc27b483e020a98a40f4b1be345b577139
paiuscatalin <p.cata95@gmail.com>
2022-08-24 10:18:01 +0300

    [Refactor] Duplicated code for the same display econsent action https://github.com/PharmaLedger-IMI/eco-iot-pmed-workspace/issues/399

--- code/scripts/controllers/SitePreviewConsentController.js
+++ code/scripts/controllers/SitePreviewConsentController.js
@@ -1,36 +1,19 @@
 // eslint-disable-next-line no-undef
 const commonServices = require('common-services');
 const BreadCrumbManager = commonServices.getBreadCrumbManager();
-const FileDownloaderService = commonServices.FileDownloaderService;
+const PDFService = commonServices.PDFService;
 
 export default class SitePreviewConsentController extends BreadCrumbManager {
   constructor(...props) {
     super(...props);
-    let { trialId, trialKeySSI, trialUid, siteId, siteKeySSI, siteUid, data, history } = this.history.location.state;
 
-    this.model = {
-      trialId,
-      trialKeySSI,
-      trialUid,
-      siteId,
-      siteKeySSI,
-      siteUid,
-      consent: data,
-      history,
-      pdf: {
-        currentPage: 1,
-        pagesNo: 0,
-      },
-      showPageUp: false,
-      showPageDown: true,
-    };
+    let {trialId, trialKeySSI, trialUid, siteId, siteKeySSI, siteUid, data, history} = this.getState();
+    this.model = {trialId, trialKeySSI, trialUid, siteId, siteKeySSI, siteUid, consent: data, history};
 
     this.model.breadcrumb = this.setBreadCrumb({
       label: `${data.name} / Preview Consent`,
       tag: `site-preview-consent`
     });
-
-    this.fileDownloaderService = new FileDownloaderService(this.DSUStorage);
 
     this.init();
   }
@@ -42,80 +25,10 @@ export default class SitePreviewConsentController extends BreadCrumbManager {
       this.model.consent.uid,
       this.model.consent.version
     );
-    this.downloadFile(econsentFilePath, this.model.consent.attachment);
+
+    this.PDFService = new PDFService(this.DSUStorage);
+    this.PDFService.displayPDF(econsentFilePath, this.model.consent.attachment);
   }
-
-  downloadFile = async (filePath, fileName) => {
-    await this.fileDownloaderService.prepareDownloadFromDsu(filePath, fileName);
-    let fileBlob = this.fileDownloaderService.getFileBlob(fileName);
-    this.rawBlob = fileBlob.rawBlob;
-    this.mimeType = fileBlob.mimeType;
-    this.blob = new Blob([this.rawBlob], {
-      type: this.mimeType,
-    });
-    this.displayFile();
-  };
-
-  loadPdfOrTextFile = () => {
-    const reader = new FileReader();
-    reader.readAsDataURL(this.blob);
-    reader.onloadend = () => {
-      let base64data = reader.result;
-      this.initPDF(base64data.substr(base64data.indexOf(',') + 1));
-    };
-  };
-
-  initPDF(pdfData) {
-    pdfData = atob(pdfData);
-    let pdfjsLib = window['pdfjs-dist/build/pdf'];
-    pdfjsLib.GlobalWorkerOptions.workerSrc = 'scripts/third-parties/pdf.worker.js';
-
-    this.loadingTask = pdfjsLib.getDocument({ data: pdfData });
-    this.renderPage(this.model.pdf.currentPage);
-    window.WebCardinal.loader.hidden = true;
-  }
-
-  renderPage = (pageNo) => {
-    this.loadingTask.promise.then(
-      (pdf) => {
-        this.model.pdf.pagesNo = pdf.numPages;
-        pdf.getPage(pageNo).then((result) => this.handlePages(pdf, result));
-      },
-      (reason) => console.error(reason)
-    );
-  };
-
-  handlePages = (thePDF, page) => {
-    const viewport = page.getViewport({ scale: 1.5 });
-    let canvas = document.createElement('canvas');
-    canvas.style.display = 'block';
-    let context = canvas.getContext('2d');
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-    page.render({ canvasContext: context, viewport: viewport });
-    document.getElementById('canvas-parent').appendChild(canvas);
-
-    this.model.pdf.currentPage = this.model.pdf.currentPage + 1;
-    let currPage = this.model.pdf.currentPage;
-    if (thePDF !== null && currPage <= this.model.pdf.pagesNo) {
-      thePDF.getPage(currPage).then((result) => this.handlePages(thePDF, result));
-    }
-  };
-
-  displayFile = () => {
-    window.URL = window.URL || window.webkitURL;
-    const fileType = this.mimeType.split('/')[0];
-    switch (fileType) {
-      case 'image': {
-        this.loadImageFile();
-        break;
-      }
-      default: {
-        this.loadPdfOrTextFile();
-        break;
-      }
-    }
-  };
 
   getEconsentManualFilePath(siteUid, consentUid, version) {
     return '/sites/' + siteUid + '/consent/' + consentUid + '/versions/' + version;
