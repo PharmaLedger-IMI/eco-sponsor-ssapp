f1fa260c0e565c828c3c80e479f08970e6754d13
Fotios Nalmpantis <fotisn@gmail.com>
2022-09-02 13:21:04 +0300

    status

--- code/scripts/controllers/ListParticipantsStatusController.js
+++ code/scripts/controllers/ListParticipantsStatusController.js
@@ -36,10 +36,12 @@ export default class ListParticipantsStatusController extends BreadCrumbManager 
     this.trialsService = new TrialsService(this.DSUStorage);
     this.sitesService = new SitesService(this.DSUStorage);
     this.participantsService = new ParticipantsService(this.DSUStorage);
-    let { participantUid, participantId, trialId, trialKeySSI, trialUid, siteKeySSI, siteId, siteUid } = this.history.location.state;
+    let { participantUid, participantPk, participantId, trialId, trialKeySSI, trialUid, siteKeySSI, siteId, siteUid } =
+      this.history.location.state;
 
     this.model = {
       participantUid,
+      participantPk,
       participantId,
       trialId,
       trialKeySSI,
@@ -56,12 +58,12 @@ export default class ListParticipantsStatusController extends BreadCrumbManager 
       clearButtonDisabled: true,
       type: 'consents',
       tableLength: 7,
-      addConsentButtonDisabled: true
+      addConsentButtonDisabled: true,
     };
 
     this.model.breadcrumb = this.setBreadCrumb({
       label: `${participantId} / Site Participant's Status`,
-      tag: `site-participant-status`
+      tag: `site-participant-status`,
     });
 
     this.attachEvents();
@@ -74,27 +76,68 @@ export default class ListParticipantsStatusController extends BreadCrumbManager 
   }
 
   async getData() {
-    // window.WebCardinal.loader.hidden = false;
-    // const model = await this.participantsService.getParticipantConsentHistory(
-    //   this.model.participantUid,
-    //   this.model.trialKeySSI,
-    //   this.model.siteKeySSI
-    // );
-    // this.model.consents = JSON.parse(JSON.stringify(model));
-    // const dataModel = model.map((x) => {
-    //   const maxVersion = Math.max.apply(
-    //     Math,
-    //     x.versions.map((o) => parseInt(o.version))
-    //   );
-    //   const maxVersionObj = x.versions.find((z) => z.version === maxVersion);
-    //   return {
-    //     ...x,
-    //     ...maxVersionObj,
-    //     versionToShow: `V${maxVersionObj.version} ${new Date(maxVersionObj.versionDate).toLocaleDateString('en-UK')}`,
-    //   };
-    // });
-    // this.model.data = JSON.parse(JSON.stringify(dataModel));
-    // window.WebCardinal.loader.hidden = true;
+    window.WebCardinal.loader.hidden = false;
+    const model = await this.participantsService.getParticipantFromDb(
+      this.model.participantUid,
+      this.model.trialKeySSI,
+      this.model.siteKeySSI
+    );
+    const consents = await this.participantsService.getParticipantConsentHistory(
+      this.model.participantUid,
+      this.model.trialKeySSI,
+      this.model.siteKeySSI
+    );
+
+    const consentsSigned = consents.filter((x) => {
+      return (
+        x.versions.filter((y) => {
+          return y.actions.filter((z) => z.name === 'Signed').length > 0;
+        }).length > 0
+      );
+    });
+
+    const rows = consentsSigned
+      .map((x) =>
+        x.versions
+          .filter((y) => {
+            return y.actions.filter((z) => z.name === 'Signed').length > 0;
+          })
+          .map((z) => ({ view: `${z.version} - ${x.trialConsentName}`, consent: { ...x, ...z } }))
+      )
+      .flat();
+
+    debugger;
+    model.consentsSigned = rows.length > 0 ? rows[0].view : '-';
+    rows.shift();
+
+    this.model.data = [
+      {
+        screenedDate: model.screenedDate || '',
+        enrolledDate: model.enrolledDate || '',
+        endOfTreatmentDate: model.endOfTreatmentDate || '',
+        completedDate: model.completedDate || '',
+        WDSFDate:
+          (model.withdrewDate !== '-' && model.withdrewDate) ||
+          (model.discontinuedDate !== '-' && model.discontinuedDate) ||
+          (model.screenFailedDate !== '-' && model.screenFailedDate) ||
+          '-',
+        reconsentRequired: !model.tpSigned,
+        consentsSigned: model.consentsSigned,
+        consent: rows.length > 0 && rows[0].consent,
+      },
+      ...rows.map((x) => ({
+        screenedDate: '',
+        enrolledDate: '',
+        endOfTreatmentDate: '',
+        completedDate: '',
+        WDSFDate: '',
+        reconsentRequired: '',
+        consentsSigned: x.view,
+        consent: x.consent,
+      })),
+    ];
+
+    window.WebCardinal.loader.hidden = true;
   }
 
   attachEvents() {
@@ -103,5 +146,19 @@ export default class ListParticipantsStatusController extends BreadCrumbManager 
       () => !!(this.model.data && Array.isArray(this.model.data) && this.model.data.length > 0),
       'data'
     );
+
+    this.onTagClick('view-participant-consent-preview', async (model) => {
+      this.navigateToPageTag('site-participant-preview', {
+        participantPk: this.model.participantPk,
+        trialId: this.model.trialId,
+        trialKeySSI: this.model.trialKeySSI,
+        trialUid: this.model.trialUid,
+        siteKeySSI: this.model.siteKeySSI,
+        siteId: this.model.siteId,
+        siteUid: this.model.siteUid,
+        consent: model.consent,
+        breadcrumb: this.model.toObject('breadcrumb'),
+      });
+    });
   }
 }
