a53b6ae62001805119717b96e2f5a1ff60da0240
mastaleru <mastaleru.rafael@gmail.com>
2022-12-22 12:18:51 +0200

    https://github.com/PharmaLedger-IMI/eco-iot-pmed-workspace/issues/544 https://github.com/PharmaLedger-IMI/eco-iot-pmed-workspace/issues/545

--- code/scripts/preload.js
+++ code/scripts/preload.js
@@ -10,7 +10,10 @@ addHook('beforeAppLoads', async () => {
     try {
         defineWebCardinalComponents();
 
+        const {DidInputController} = await import("../components/did-input/DidInputController.js");
         await import("../components/share-did/share-did.js");
+        await import("../components/did-input/did-input.js");
+        addControllers({DidInputController});
 
     } catch (error) {
         console.error('Error while defining WebCardinal components', error);
